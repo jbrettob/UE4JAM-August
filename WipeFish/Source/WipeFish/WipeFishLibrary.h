68bc0f44910e5874e3d84137ca8b84f9e332c809
Jayce Rettob <jayce@jbrettob.com>
2016-08-13 12:55:17 +0200

    Initial commit
    Added basic template.

---
+++ WipeFish/Source/WipeFish/WipeFishLibrary.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "WipeFishLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WIPEFISH_API UWipeFishLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
